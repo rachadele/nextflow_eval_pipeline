11f425506658b0a79a6ff06f1f50bed7ab98c5a1
rachadele <raschwaa@gmail.com>
2025-06-26 19:23:41 -0700

    map valid labels before evaluating

--- bin/classify_all.py
+++ bin/classify_all.py
@@ -151,6 +151,8 @@ def main():
     
     # Classify cells and evaluate
     query = classify_cells(query, ref_keys, cutoff=cutoff, probabilities=prob_df, mapping_df=mapping_df)
+    query = map_valid_labels(query, ref_keys, mapping_df)  
+
     outdir = os.path.join("predicted_meta")
     os.makedirs(outdir, exist_ok=True)
     query.to_csv(os.path.join(outdir,f"{query_name}_{ref_name}.predictions.{cutoff}.tsv"), index=False, sep="\t")
