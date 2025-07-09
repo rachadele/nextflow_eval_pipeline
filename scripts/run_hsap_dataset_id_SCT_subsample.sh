9d0d6dbb64fffd67cd469ff711b1701affb8f134
rachadele <raschwaa@gmail.com>
2025-07-08 18:11:58 -0700

    update scripts

--- scripts/run_hsap_dataset_id_SCT_subsample.sh
+++ scripts/run_hsap_dataset_id_SCT_subsample.sh
@@ -8,26 +8,24 @@ subsample_query=100
 ref_split_values=("dataset_id")
 cutoff_values=(0 0.05 0.1 0.15 0.2 0.25 0.5 0.75)
 normalization_method="SCT"
-# not sure if I should handle this now or later
 # Loop over parameter combinations
 for subsample_ref in "${subsample_ref_values[@]}"; do
     for ref_split in "${ref_split_values[@]}"; do
         for cutoff in "${cutoff_values[@]}"; do
                 echo "Running: subsample_ref=$subsample_ref, ref_split=$ref_split, cutoff=$cutoff, normalization_method=$normalization_method"
-                nextflow main.nf -params-file /space/grp/rschwartz/rschwartz/nextflow_eval_pipeline/params.mm.json \
-                    --subsample_query "$subsample_query" \
+                nextflow main.nf -params-file /space/grp/rschwartz/rschwartz/nextflow_eval_pipeline/params.hs.json \
                     --subsample_ref "$subsample_ref" \
                     --ref_split "$ref_split" \
                     -profile conda \
                     --cutoff "$cutoff" \
                     --subset_type sample \
-                    -work-dir mmus \
+                    -work-dir hsap \
                     --batch_correct true \
                     -resume \
-                    --remove_unknown \
                     --normalization_method "$normalization_method" \
-                    --outdir_prefix "mus_musculus/subsample_query_100/old_refs" \
-                    -process.executor "slurm"
+                    --outdir_prefix "homo_sapiens/subsample_100/SCT" \
+                    --subsample_query "$subsample_query"                 
             done
         done
     done
+
