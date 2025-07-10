4a537ee3f47ba41781586c1a9ea76cc340b42f96
rachadele <raschwaa@gmail.com>
2025-07-10 12:34:15 -0700

    femove outdir prefix

--- scripts/run_hsap_collection_name_SCT_subsample.sh
+++ scripts/run_hsap_collection_name_SCT_subsample.sh
@@ -5,7 +5,7 @@ set -e  # Exit on error
 # Define parameter values
 subsample_ref_values=(50 100 500)
 subsample_query=100
-ref_split_values=("dataset_id")
+ref_split_values=("collection_name")
 cutoff_values=(0 0.05 0.1 0.15 0.2 0.25 0.5 0.75)
 normalization_method="SCT"
 # Loop over parameter combinations
@@ -23,6 +23,7 @@ for subsample_ref in "${subsample_ref_values[@]}"; do
                     --batch_correct true \
                     -resume \
                     --normalization_method "$normalization_method" \
+                    --outdir_prefix "homo_sapiens/subsample_100/SCT" \
                     --subsample_query "$subsample_query"                 
             done
         done
