7539fb2e99c970aa3a3599fc41d3e53a7dd380f5
rachadele <raschwaa@gmail.com>
2025-09-09 17:11:03 -0700

    add tests

---
+++ scripts/run_mmus_test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e  # Exit on error
+
+# Define parameter values
+subsample_ref_values=(500)
+subsample_query=100
+ref_split_values=("dataset_id")
+cutoff_values=(0)
+normalization_method="SCT"
+# not sure if I should handle this now or later
+# Loop over parameter combinations
+for subsample_ref in "${subsample_ref_values[@]}"; do
+    for ref_split in "${ref_split_values[@]}"; do
+        for cutoff in "${cutoff_values[@]}"; do
+                echo "Running: subsample_ref=$subsample_ref, ref_split=$ref_split, cutoff=$cutoff, normalization_method=$normalization_method"
+                nextflow main.nf -params-file /space/grp/rschwartz/rschwartz/nextflow_eval_pipeline/params.mm.test.json \
+                    --subsample_query "$subsample_query" \
+                    --subsample_ref "$subsample_ref" \
+                    --ref_split "$ref_split" \
+                    -profile conda \
+                    --cutoff "$cutoff" \
+                    --subset_type sample \
+                    -work-dir mmus \
+                    --batch_correct true \
+                    -resume \
+                    --remove_unknown \
+                    --normalization_method "$normalization_method" \
+                    --outdir_prefix "tests/2025-01-30/mus_musculus/use-new-census-2" \
+                    -process.executor "slurm"
+            done
+        done
+    done
