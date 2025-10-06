a1064eaedf6d45088ec8abb1701a9fc62dd842f9
rachadele <raschwaa@gmail.com>
2025-10-06 14:13:30 -0700

    update scripts

--- scripts/run_mmus_dataset_id_SCT_subsample.sh
+++ scripts/run_mmus_dataset_id_SCT_subsample.sh
@@ -26,8 +26,9 @@ for subsample_ref in "${subsample_ref_values[@]}"; do
                     -resume \
                     --remove_unknown true \
                     --normalization_method "$normalization_method" \
-                    -process.executor slurm
-                   
+                    -process.executor slurm \
+                    --use_gap false \
+                
             done
         done
     done
